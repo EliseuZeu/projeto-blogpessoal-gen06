ed4ef43dd9665f9b830323a8ecf8ef048b858780
EliseuZz <100594790+EliseuZeu@users.noreply.github.com>
2025-01-15 14:26:18 -0300

    atualizando erros dos temas replicados.

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -1,46 +1,48 @@
+import { Tema } from './../entities/tema.entity';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { TemaService } from "../services/tema.service";
 
-@Controller("/tema")
-export class temaController{
+@Controller("/temas")
+export class TemaController{
 
     constructor(
-        private readonly TemaService: TemaService
+        private readonly temaService: TemaService
     ){}
 
     @Get()
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Tema[]>{
-        return this.TemaService.findAll();
+        return this.temaService.findAll();
     }
     
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe) id: number): Promise<Tema>{
-        return this.TemaService.findById(id);
+        return this.temaService.findById(id);
     }
 
     @Get('/descricao/:descricao')
     @HttpCode(HttpStatus.OK)
     findBydescricao(@Param('descricao') descricao: string): Promise<Tema[]>{
-        return this.TemaService.findByDescricao(descricao);
+        return this.temaService.findByDescricao(descricao);
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     create(@Body() Tema: Tema): Promise<Tema>{
-        return this.TemaService.create(Tema);
+        return this.temaService.create(Tema);
     }
 
     @Put()
     @HttpCode(HttpStatus.OK)
     update(@Body() Tema: Tema): Promise<Tema>{
-        return this.TemaService.update(Tema);
+        return this.temaService.update(Tema);
     }
 
     @Delete('/:id')
     @HttpCode(HttpStatus.NO_CONTENT)
     delete(@Param('id', ParseIntPipe) id: number){
-        return this.TemaService.delete(id);
+        return this.temaService.delete(id);
     }
 
 }
